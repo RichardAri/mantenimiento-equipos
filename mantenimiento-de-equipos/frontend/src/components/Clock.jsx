d21b0c378f7cb8ef84be6a80c353c5715469f2d1
RichardAri <richardaripari@gmail.com>
2024-07-15 16:20:51 -0500

    Se corrigio el estilo del reloj del login y el login

--- mantenimiento-de-equipos/frontend/src/components/Clock.jsx
+++ mantenimiento-de-equipos/frontend/src/components/Clock.jsx
@@ -13,7 +13,7 @@ const Clock = () => {
     }, []);
 
     const formatTime = (date) => {
-        return date.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
     };
 
     const formatDate = (date) => {
