2de85172e412c737f5469c99e9130d75f721e1de
RichardAri <richardaripari@gmail.com>
2024-07-15 11:09:13 -0500

    Se añadio componente de Tiendas

---
+++ mantenimiento-de-equipos/frontend/src/components/ListaTiendas.jsx
@@ -0,0 +1,13 @@
+// src/components/ListaTiendas.jsx
+import React from 'react';
+
+const ListaTiendas = () => {
+  return (
+    <div>
+      <h1>Lista de Tiendas</h1>
+      {/* Aquí iría tu lógica para mostrar la lista de tiendas */}
+    </div>
+  );
+};
+
+export default ListaTiendas;
