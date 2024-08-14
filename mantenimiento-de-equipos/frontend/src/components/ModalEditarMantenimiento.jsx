17b8da3f693f927bf51838996b2d9ccae2c1f398
RichardAri <richardaripari@gmail.com>
2024-08-14 11:14:19 -0500

    Se añadio las clases de css necesarias para el correcto funcionamiento de el modal

--- mantenimiento-de-equipos/frontend/src/components/ModalEditarMantenimiento.jsx
+++ mantenimiento-de-equipos/frontend/src/components/ModalEditarMantenimiento.jsx
@@ -44,25 +44,26 @@ const ModalEditarMantenimiento = ({ isOpen, onRequestClose, mantenimiento, onSav
 
   return (
     <Modal isOpen={isOpen} onRequestClose={onRequestClose} className="modal" overlayClassName="modal-overlay">
-      <h2>Editar Mantenimiento</h2>
+      <h2 className='add-subtitle'>Editar Mantenimiento</h2>
       <form onSubmit={handleSubmit}>
         <label className="readonly-label">Código del Equipo:</label>
-        <input type="text" value={codigoEquipo} readOnly className="readonly-input" />
-        <label>Nombre:</label>
-        <select value={nombre} onChange={(e) => setNombre(e.target.value)}>
+        <input type="text" value={codigoEquipo} readOnly className="readonly-input"/>
+        <label className='form-lbl-text'> Nombre:</label>
+        <select className='form-lbl-text ' value={nombre} onChange={(e) => setNombre(e.target.value)}>
           <option value="Cambio de Disco">Cambio de Disco</option>
           <option value="Cambio de S.O.">Cambio de S.O.</option>
           <option value="Cambio de Ram">Cambio de Ram</option>
           <option value="Mantenimiento General">Mantenimiento General</option>
         </select>
-        <label>Descripción:</label>
-        <textarea value={descripcion} onChange={(e) => setDescripcion(e.target.value)} required />
-        <h3>Cambios</h3>
-        <label>Nombre del Equipo:</label>
+        <label className='container-txta'>Descripción:
+          <textarea value={descripcion} onChange={(e) => setDescripcion(e.target.value)} required className='auto-textarea'/>
+        </label>
+        <h3 >Cambios</h3>
+        <label className='form-lbl-text'>Nombre del Equipo:</label>
         <input type="text" value={nombreEquipo} onChange={(e) => setNombreEquipo(e.target.value)} required />
-        <label>IP del Equipo:</label>
+        <label className='form-lbl-text'>IP del Equipo:</label>
         <input type="text" value={ipEquipo} onChange={(e) => setIpEquipo(e.target.value)} required />
-        <label>Descripción del Equipo:</label>
+        <label className='form-lbl-text'>Descripción del Equipo:</label>
         <textarea value={descripcionEquipo} onChange={(e) => setDescripcionEquipo(e.target.value)} required />
         <div className="button-group">
           <button type="submit" className="save-button">Guardar</button>
