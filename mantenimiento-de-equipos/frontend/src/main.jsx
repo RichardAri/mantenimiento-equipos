be2fa3b070325d261cdd8fabe9ce34334ff67048
RichardAri <richardaripari@gmail.com>
2024-11-15 09:47:50 -0500

    comentarios mas descriptivos

--- mantenimiento-de-equipos/frontend/src/main.jsx
+++ mantenimiento-de-equipos/frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Aquí asegúrate de usar la API correcta
+import ReactDOM from 'react-dom/client'; // API 
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './components/App'; // Tu componente principal
-import { AuthProvider } from './context/AuthContext'; // Ajusta la ruta si es necesario
+import App from './components/App'; // componente principal
+import { AuthProvider } from './context/AuthContext'; // Provider
 
 // Crear el root con React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
